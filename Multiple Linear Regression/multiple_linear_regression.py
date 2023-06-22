37bdf67da1eeea8651c18fd451d75efce5cdd2cf
aksh-nishar <akshnishar18@gmail.com>
2023-06-22 17:20:42 +0530

    :sparkles: Add assets, code for multiple linear regression.
    Added 50 startups csv data file, multiple linear regression notes,
    screenshot of assumptions of linear regression and the code to implement
    the multiple linear regression model.

---
+++ Multiple Linear Regression/multiple_linear_regression.py
@@ -0,0 +1,32 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Multiple Linear Regression/50_Startups.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+# print(x)
+# print(y)
+
+# Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+# print(x)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+# Traininig the Multiple Linear Regression Model on the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(x_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)), 1))
